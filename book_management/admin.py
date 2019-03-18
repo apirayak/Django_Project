b5571a789d58f8175df116d9de94109f03040b32
styley997 <styley997@gmail.com>
2019-03-18 19:13:43 +0700

    Book management show in admin site

--- book_management/admin.py
+++ book_management/admin.py
@@ -2,4 +2,16 @@ from django.contrib import admin
 
 from book_management.models import Book,Category
 
-admin.site.register(Book,Category)
+class BookInline(admin.TabularInline):
+    model = Book
+
+class CategoryAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['name']})
+    ]
+    search_fields = ['name']   
+    list_filter = ['name']
+    inlines = [BookInline]
+    Category_display = ('name')
+
+admin.site.register(Category, CategoryAdmin)
